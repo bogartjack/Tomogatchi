82ad8a210379aeb29ba5847afab54c756d5bd553
Jack Bogart <bogart.jack98@gmail.com>
2018-09-28 09:52:09 -0600

    Redid the basic layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let sleepiness =1;
 let boredom =1;
 let hunger =1;
+let age =0;
 
 class Tomagotchi{
 	constructor(name){
@@ -15,7 +16,9 @@ setInterval( () => {
 	sleepiness = sleepiness*1.15;
 	boredom = boredom*1.25;
 	hunger = hunger*1.35;
-	
+	age = 500/3000;
+	grow();
+	age(age);
 	sleep(sleepiness);
 	bored(boredom);
 	hungry(hunger);
@@ -24,7 +27,7 @@ setInterval( () => {
 
 
 const sleep = () =>{
-
+	
 }
 
 const bored = () =>{
@@ -37,6 +40,7 @@ const hungry = () =>{
 
 const startGame = () =>{
 	let name = prompt('Give your gotch a name', 'Name');
+	$('#tomogatchi').html('<circle cx ="25" cy ="25" r="25" fill ="red"></circle>');
 	const tom = new Tomagotchi(name);
 
 }
