14c6b1b07c2af372cc4681e8e1cfcecdb18b7997
Jack Bogart <bogart.jack98@gmail.com>
2018-09-28 10:24:15 -0600

    Added growth functionality

--- app.js
+++ app.js
@@ -12,38 +12,43 @@ class Tomagotchi{
 	}
 }
 
-setInterval( () => {
-	sleepiness = sleepiness*1.15;
-	boredom = boredom*1.25;
-	hunger = hunger*1.35;
-	age = 500/3000;
-	grow();
-	age(age);
+setInterval( play = () => {
+	sleepiness = sleepiness*1.1;
+	boredom = boredom*1.15;
+	hunger = hunger*1.2;
+	age += 500/3000;
+	grow(age);
 	sleep(sleepiness);
 	bored(boredom);
 	hungry(hunger);
 
 }, 500)
 
-
-const sleep = () =>{
-	
+const grow = (size) =>{
+	$('#age').css('height', size + 5 + '%');
 }
 
-const bored = () =>{
-
+const sleep = (slp) =>{
+	$('#sleepiness').css('height', slp + 5 + '%');	
 }
 
-const hungry = () =>{
+const bored = (brd) =>{
+	$('#boredom').css('height', brd + 5 + '%');
+}
 
+const hungry = (hng) =>{
+	$('#hunger').css('height', hng + 5 + '%');
 }
 
 const startGame = () =>{
 	let name = prompt('Give your gotch a name', 'Name');
 	$('#tomogatchi').html('<circle cx ="25" cy ="25" r="25" fill ="red"></circle>');
 	const tom = new Tomagotchi(name);
+	$('#intro').html('<p>make sure to feed, play, or put ' + tom.name + ' to sleep</p><p>He will grow if you keep him alive!</p>');
+	play();
 
 }
+
 
 
 
