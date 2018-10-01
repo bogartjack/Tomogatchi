d8bf8790a65670db98b3e2af597067a4c36dd20e
Jack Bogart <bogart.jack98@gmail.com>
2018-10-01 12:54:20 -0600

    Final commit, evolution and growth functionality added

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const interval = (flag) => {
 		sleepiness = sleepiness*1.1;
 		boredom = boredom*1.15;
 		hunger = hunger*1.2;
-		age += 500/3000;
+		age += 500/2000;
 		grow(age);
 		sleep(sleepiness);
 		bored(boredom);
@@ -32,10 +32,50 @@ const interval = (flag) => {
 	else { clearInterval(intervalID); }
 }
 
+const evolveNotify = () => {
+	
+	if (age > 10 && age < 11){
+		$('#notification').text('Your gotch is evolving to level 2');
+	}
+	else if (age > 20 && age < 21){
+		$('#notification').text('Your gotch is evolving to level 3');
+	}
+	else if (age > 30 && age < 31){
+		$('#notification').text('Your gotch is evolving to level 4');
+	}
+	else if (age > 40 && age <41){
+		$('#notification').text('Your gotch is reaching its last level!');
+	}
+}
+
+const evolve = (clicked) => {
+	if (age > 10 && age < 20){
+		$('#gotch').attr('fill', 'pink');
+	}
+	else if (age > 20 && age < 30){
+		$('#gotch').attr('fill', 'purple');
+	}
+	else if (age > 30 && age < 40){
+		$('#gotch').attr('fill', 'black');
+	}
+	else if (age > 40 && age <50){
+		$('#gotch').attr('fill', 'yellow');
+	}
+	else if (age > 50){
+		alert('Your gotch has evolved completely! Press okay to restart!"');
+		restartGame(false);
+	}
+
+}
+
 const grow = (size) =>{
 //	console.log(size);
 	$('#age').css('height', (size +5)+ '%');
 	$('#age').text(Math.floor((size)));
+	$('#tomogatchi').attr('height', 50 + size).attr('width', 50 + size).attr('left', '50%').attr('top', '50%').css('transform', 'translate(-50%, -50%)');
+	$('#gotch').attr('cx', 25 + (size/2)).attr('cy',25+(size/2)).attr('r',25+(size/2));
+	evolveNotify();
+	evolve();
 }
 
 const sleep = (slp) =>{
@@ -45,7 +85,7 @@ const sleep = (slp) =>{
 	$('#sleepiness').css('height', (slp) +5+ '%');	
 	$('#sleepiness').text(Math.floor(sleepinessLevel)+1);
 	if (slp > 85){
-		restartGame();
+		restartGame(true);
 	}
 }
 
@@ -55,7 +95,7 @@ const bored = (brd) =>{
 	$('#boredom').css('height', (brd) + 5 + '%');
 	$('#boredom').text(Math.floor(boredomLevel)+1);
 	if (brd > 85){
-		restartGame();
+		restartGame(true);
 	}
 }
 
@@ -66,13 +106,18 @@ const hungry = (hng) =>{
 	$('#hunger').css('height', (hng) + 5 + '%');
 	$('#hunger').text(Math.floor(hungerLevel)+1);
 	if (hng>85){
-		restartGame();
+		restartGame(true);
 	}
 }
 
-const restartGame = () => {
+const restartGame = (death) => {
 	interval(false);
-	alert('You let your gotch die! Press okay to try again!');
+	if (death){
+		alert('You let your gotch die! Press okay to try again!');
+	}
+	else {
+		alert('congrats on winning the game!');
+	}
 	sleepiness =1;
 	boredom =1;
 	hunger =1;
@@ -81,11 +126,16 @@ const restartGame = () => {
 	boredomLevel = 0;
 	hungerLevel = 0;
 	interval(true);
+	$('#tomogatchi').attr('height', '50').attr('width', '50');
+	$('#gotch').attr('cx', '25').attr('cy', '25').attr('r', '25');
+}
+
+const gotchNotification = (text, gotchName) => {
+	$('#notification').text(`You have already ${text} ${gotchName} enough`);
 }
 
 const interact = (gotchName) => {
 	
-	console.log('doohickey');
 	$('#hunger-button').on('click', () => {
 		if(hunger > 8.5){
 			hunger-=8.5;
@@ -93,7 +143,7 @@ const interact = (gotchName) => {
 		}
 		
 		else{
-
+			gotchNotification('fed', gotchName);
 		}
 	});
 	$('#sleep-button').on('click', () => {
@@ -103,7 +153,7 @@ const interact = (gotchName) => {
 		}
 		
 		else{
-
+			gotchNotification(`let ${gotchName} sleep enough`, '');
 		}
 	});
 	$('#bored-button').on('click', () => {
@@ -113,20 +163,22 @@ const interact = (gotchName) => {
 		}
 		
 		else{
-	
+			gotchNotification('played with', gotchName);	
 
 		}
 	});
-	('#evolve-button').on('click', () => {
+}
 
-	});
+const SVG = (tag) => {
+	return document.createElementNS('http://www.w3.org/2000/svg', tag);
 }
 
 const startGame = () =>{
+	const $gotch = $('#tomogatchi');
 	let name = prompt('Give your gotch a name', 'Name');
-	$('#tomogatchi').html('<circle cx ="25" cy ="25" r="25" fill ="red"></circle>');
+	$(SVG('text')).attr('id', 'gotchText').attr('x','50%').attr('y','50%').attr('text-anchor','middle').attr('alignment-baseline', 'middle').html(name).appendTo($gotch);
 	const tom = new Tomagotchi(name);
-	$('#intro').html('<p>make sure to feed, play, or put ' + tom.name + ' to sleep<br/></p><p>He will grow if you keep him alive!</p>');
+	$('#intro').html('<p>make sure to feed, play, or put ' + tom.name + ' to sleep<br/></p><p id ="notification">He will grow if you keep him alive!</p>');
 	interval(true);
 	interact(tom.name);
 
